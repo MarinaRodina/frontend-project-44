328904af1211189557899872c72d2b220df807a2
marina rodina <mrodina1990@yandex.ru>
2022-12-02 17:48:06 +0500

    fix method for getting random number

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,7 @@ const checkNumberIsEven = () => {
   const noteToEven = 'Answer "yes" if the number is even, otherwise answer "no".';
   const taskEven = () => {
     const maxNumberToCheck = 100;
-    const getRandomNumber = () => (Math.round(Math.random() * maxNumberToCheck));
+    const getRandomNumber = () => (Math.floor(Math.random() * maxNumberToCheck));
     const number = getRandomNumber();
     const isEven = (num) => num % 2 === 0;
     const question = `${number}`;
