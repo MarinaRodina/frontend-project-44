aebce17459f0957c792115f43c5a62c6b6237ae7
marina rodina <mrodina1990@yandex.ru>
2022-12-05 17:50:55 +0500

    add brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,27 @@
+import engine from '../index.js';
+import { getRandomNumber } from '../helperFunctions.js';
+
+const checkNumberIsPrime = () => {
+  const noteToPrime = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const taskPrime = () => {
+    const number = getRandomNumber(50);
+    const isPrime = (num) => {
+      if (num < 2) {
+        return false;
+      }
+      let divisor = 2;
+      while (divisor <= num / 2) {
+        if (num % divisor === 0) {
+          return false;
+        }
+        divisor += 1;
+      }
+      return true;
+    };
+    const question = `${number}`;
+    const result = (isPrime(number) ? 'yes' : 'no');
+    return [question, result];
+  };
+  engine(noteToPrime, taskPrime);
+};
+export default checkNumberIsPrime;
