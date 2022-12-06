02ae589c4289fd91d520ad3473040260cd23adef
marina rodina <mrodina1990@yandex.ru>
2022-12-06 16:53:55 +0500

    fix function getNumberArray

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,11 +5,11 @@ const getMissingNumber = () => {
   const noteProgression = 'What number is missing in the progression?';
   const taskProgression = () => {
     const number = getRandomNumber(50);
+    const step = getRandomNumber(5);
     const getNumberArray = () => {
       const numberArray = [];
       let num = number;
       let count = 1;
-      const step = getRandomNumber(5);
       while (count <= 10) {
         numberArray.push(num);
         num += step;
