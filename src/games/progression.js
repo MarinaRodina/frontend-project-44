0ae52ef6b447d59fdff677572fa33416661a1425
marina rodina <mrodina1990@yandex.ru>
2022-12-06 18:41:53 +0500

    move the minor feature out of the main feature

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,30 +1,33 @@
 import startGame from '../index.js';
 import getRandomNumber from '../helperFunctions.js';
 
+const getNumberArray = (num1, step1) => {
+  const numberArray = [];
+  let num = num1;
+  let count = 1;
+  while (count <= 10) {
+    numberArray.push(num);
+    num += step1;
+    count += 1;
+  }
+  return numberArray;
+};
+
 const startGameBrainProgression = () => {
   const noteBrainProgression = 'What number is missing in the progression?';
   const decideBrainProgression = () => {
     const number = getRandomNumber(50);
     const step = getRandomNumber(5);
-    const getNumberArray = () => {
-      const numberArray = [];
-      let num = number;
-      let count = 1;
-      while (count <= 10) {
-        numberArray.push(num);
-        num += step;
-        count += 1;
-      }
-      return numberArray;
-    };
-    const array = getNumberArray();
+    const array = getNumberArray(number, step);
     const randomIndex = getRandomNumber(8);
     const hiddenNumber = array[randomIndex];
     const firstPartArrya = array.slice(0, randomIndex);
     const secondPartArrya = array.slice(randomIndex + 1);
     const firstPartNumbers = firstPartArrya.join(' ');
     const secondParNumbers = secondPartArrya.join(' ');
-    return [`${firstPartNumbers} .. ${secondParNumbers}`, hiddenNumber.toString()];
+    const result = hiddenNumber.toString();
+    const question = `${firstPartNumbers} .. ${secondParNumbers}`;
+    return [question, result];
   };
   startGame(noteBrainProgression, decideBrainProgression);
 };
